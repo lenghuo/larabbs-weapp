cba988d04bd0566f2e80fd7eb629c592f3ba0ba4
lenghuo <weixupeng999@163.com>
2018-04-30 16:07:42 +0800

    update avatar

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,5 @@
 import wepy from 'wepy'
+import { asyncInc } from '../store/actions/counter'
 
 // 服务器接口地址
 const host = 'http://larabbs.test/api'
@@ -122,10 +123,28 @@ const logout = async (params = {}) => {
   }
   return logoutResponse
 }
+// 上传文件
+const updateFile = async (options = {}) => {
+  // 显示loading
+  wepy.showLoading({title: '上传中'})
+  // 获取token
+  let accessToken = await getToken()
+  // 拼接url
+  options.url = host + '/' + options.url
+  let header = options.header || {}
+  header.Authorization = 'Bearer' + accessToken
+  options.header = header
+  // 上传文件
+  let response = await wepy.uploadFile(options)
+  // 隐藏loading
+  wepy.hideLoading()
+  return response
+}
 export default {
   request,
   refreshToken,
   authRequest,
+  updateFile,
   login,
   logout
 }
